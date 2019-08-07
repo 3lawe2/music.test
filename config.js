dc874103b89b13a7ed903de2d5e61dbce6d59c45
3lawe2 <53731740+3lawe2@users.noreply.github.com>
2019-08-07 07:17:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjA4NTExMjE3MDE0ODAwNDE0.XUpORw.8kYw4UCL8hdHIsdmKU6Q1Cveem8', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: 'Splay',
+    devs: [556132410350239756']
+}
